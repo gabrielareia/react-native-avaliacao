0a666bfb5ebc442f3d44adc2c5d797454a8f9117
Gabriel Coutinho Areia <gabrielareia3d@gmail.com>
2022-04-25 15:11:30 -0300

    Change yarnLock and disable logbox

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { LogBox } from 'react-native';
 import 'react-native-gesture-handler';
 import Application from './src/Application';
 import LocalizationProvider from './src/context/localization';
@@ -7,6 +8,7 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 
 export default function App() {
   const queryClient = new QueryClient();
+  LogBox.ignoreAllLogs();
   return (
     <QueryClientProvider client={queryClient}>
       <StateProvider>
